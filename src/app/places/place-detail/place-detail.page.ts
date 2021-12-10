9a875a4c0965b401b07068a4deeba36cfa14931c
jlaterza <laterzajulian@gmail.com>
2021-12-09 23:06:32 -0300

    alerta botón eliminar

--- src/app/places/place-detail/place-detail.page.ts
+++ src/app/places/place-detail/place-detail.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router' //El router te permite navegar
+import {ActivatedRoute, Router} from '@angular/router' //El router te permite navegar
 import { Place } from '../place.model';
+import {AlertController} from "@ionic/angular";
 import { PlacesService } from '../places.service';
+
 
 
 @Component({
@@ -12,7 +14,8 @@ import { PlacesService } from '../places.service';
 export class PlaceDetailPage implements OnInit {
   place: Place
 
-  constructor(private activateRoute: ActivatedRoute, private placesService: PlacesService) { }
+  constructor(private activateRoute: ActivatedRoute, private placesService: PlacesService, private router: Router, private alertCtrl: AlertController) { }
+  // Con private router: Router, lo que hago es aplicar el método para redirigir Aplicable en Delete
 
   ngOnInit() { //Con este código, tomo todos los datos del ID
     this.activateRoute.paramMap.subscribe(paraMap => {
@@ -23,7 +26,24 @@ export class PlaceDetailPage implements OnInit {
     })
   }
 
-  deletePlace() {
-    this.placesService.deletePlace(this.place.id);
+  async deletePlace() {
+
+    const alertElement = await this.alertCtrl.create({
+      header: 'Desea eliminar el lugar?',
+      message: "Si indicas 'Ok', se eliminará.",
+      buttons: [{
+        text: 'Cancelar',
+        role: 'cancel',
+      },
+      {
+        text: 'Ok',
+        handler: () => {
+          this.placesService.deletePlace(this.place.id);
+          this.router.navigate(['/places']) // Cada vez que elimino un elemento, redigir a la página principal
+        }
+      }
+    ]
+    });
+    await alertElement.present();
   }
 }
